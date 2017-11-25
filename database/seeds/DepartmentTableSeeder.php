7e5cc75542944aaeda71ca703db0cdec5895badb
badueny <33746071+badueny@users.noreply.github.com>
2017-11-25 18:26:28 +0700

    Update DepartmentTableSeeder.php

--- database/seeds/DepartmentTableSeeder.php
+++ database/seeds/DepartmentTableSeeder.php
@@ -12,34 +12,11 @@ class DepartmentTableSeeder extends Seeder
      */
     public function run()
     {
-        $department = new Departments();
-        $department->name = 'Wordpress';
-        $department->save();
-
-        $department = new Departments();
-        $department->name = 'Sales';
-        $department->save();
-
-        $department = new Departments();
-        $department->name = 'Support ';
-        $department->save();
-
-        $department = new Departments();
-        $department->name = 'Envato ';
-        $department->save();
-
+        
         $department = new Departments();
         $department->name = 'Services ';
         $department->save();
-
-        $department = new Departments();
-        $department->name = 'Designing ';
-        $department->save();
-
-        $department = new Departments();
-        $department->name = 'SEO';
-        $department->save();
-
+        
         $department = new Departments();
         $department->name = 'Marketing';
         $department->save();
