49db51013f862f005107a1a000b78b333e001f02
badueny <33746071+badueny@users.noreply.github.com>
2017-11-25 18:24:58 +0700

    Update SettingsTableSeeder.php

--- database/seeds/SettingsTableSeeder.php
+++ database/seeds/SettingsTableSeeder.php
@@ -13,20 +13,20 @@ class SettingsTableSeeder extends Seeder
     public function run()
     {
         $settings = new Settings();
-        $settings->name = 'Ticket Plus';
+        $settings->name = 'Helpdesk';
         $settings->logo = 'logo.png';
         $settings->footer_logo = 'footer-logo.png';
         $settings->description = '';
-        $settings->footer_description = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has';
-        $settings->copyrights = '© Copyrights 2017 Ticket Plus All rights reserved';
+        $settings->footer_description = 'Helpdesk';
+        $settings->copyrights = '© Copyrights 2017 All rights reserved';
         $settings->keywords = '';
-        $settings->facebook = 'https://www.facebook.com/';
-        $settings->twitter = 'https://www.twitter.com/';
-        $settings->linkedin = 'https://www.linkedin.com/';
+        $settings->facebook = '';
+        $settings->twitter = '';
+        $settings->linkedin = '';
         $settings->staff_can_edit = 'yes';
         $settings->client_can_edit = 'yes';
         $settings->ticket_email = 'no';
-        $settings->admin_email = 'nd_sohail@yahoo.com';
+        $settings->admin_email = 'badueny@gmail.com';
         $settings->save();
     }
 }
