df2be045bed675b9c18b62e69b2c13326b66ffb2
badueny <33746071+badueny@users.noreply.github.com>
2017-11-25 19:05:02 +0700

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,41 +15,41 @@ class UsersTableSeeder extends Seeder
     {
         $role1 = new Role();
         $role1->name = 'admin';
-        $role1->display_name = 'admin';
+        $role1->display_name = 'administrator';
         $role1->save();
 
         $role2 = new Role();
-        $role2->name = 'client';
-        $role2->display_name = 'client';
+        $role2->name = 'pengguna';
+        $role2->display_name = 'pengguna';
         $role2->save();
 
         $role3 = new Role();
-        $role3->name = 'staff';
-        $role3->display_name = 'staff';
+        $role3->name = 'operator';
+        $role3->display_name = 'operator';
         $role3->save();
         
         $user = new User();
-        $user->name = 'Admin';
-        $user->email = 'admin@adminx.com';
+        $user->name = 'Administrator';
+        $user->email = 'adminis@adminx.com';
         $user->username = 'admin';
-        $user->password = bcrypt('123456!@#');
+        $user->password = bcrypt('87654321');
         $user->save();
         $user->roles()->attach($role1->id);
 
         $user2 = new User();
-        $user2->name = 'client';
-        $user2->email = 'client@clientx.com';
-        $user2->username = 'client';
-        $user2->password = bcrypt('123456!@#');
+        $user2->name = 'pengguna';
+        $user2->email = 'pengguna@penggunax.com';
+        $user2->username = 'pengguna';
+        $user2->password = bcrypt('87654321');
         $user2->save();
         $user2->roles()->attach($role2->id);
 
         $user3 = new User();
-        $user3->name = 'staff';
-        $user3->email = 'staff@staffx.com';
-        $user3->username = 'staff';
-        $user3->password = bcrypt('123456!@#');
-        $user3->designation = 'Services';
+        $user3->name = 'operator';
+        $user3->email = 'operator@operatorx.com';
+        $user3->username = 'operator';
+        $user3->password = bcrypt('87654321');
+        $user3->designation = 'Layanan';
         $user3->save();
         $user3->roles()->attach($role3->id);
     }
